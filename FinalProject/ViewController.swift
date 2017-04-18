7b55056d278a49a2410abec8ee42934f04142592
Ji Hwan Seung <jiseung95@gmail.com>
2017-04-17 23:11:23 -0400

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Ji Hwan Seung on 17/04/2017.
+//  Copyright © 2017 Ji Hwan Seung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
